676c97b7645bd1ea594c12928de2f5d705e76c60
unknown <yerassylgumarov@gmail.com>
2023-04-08 02:00:45 +0600

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿static int FindMin(int[] nums, int n)
+{
+    if (n == 1)
+    {
+        return nums[0];
+    }
+    else
+    {
+        int min = FindMin(nums, n - 1);
+        if (nums[n - 1] < min)
+        {
+            return nums[n - 1];
+        }
+        else
+        {
+            return min;
+        }
+    }
+}
+
+
+    int n = int.Parse(Console.ReadLine());
+    int[] nums = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        nums[i] = int.Parse(Console.ReadLine());
+    }
+
+    int min = FindMin(nums, n);
+    Console.WriteLine(min);
